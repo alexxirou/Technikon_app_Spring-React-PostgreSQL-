1b719359d8576bfea2534813d064e57498fc07a2
Alexandros Xirouchakis <alexandrosx@ISIH.LOCAL>
2024-05-05 14:18:56 +0300

    Added optimistic locking exception failure for concurrent writes in the db during operations

--- Technikon-App-BackEnd/src/main/java/com/scytalys/technikon/exception/GlobalExceptionHandler.java
+++ Technikon-App-BackEnd/src/main/java/com/scytalys/technikon/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.scytalys.technikon.exception;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.dao.DataAccessResourceFailureException;
+import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -35,6 +36,13 @@ public class GlobalExceptionHandler {
         headers.add("Error-Message", "Database resource access failed: " + e.getMessage());
 
         return new ResponseEntity<>(null, headers, HttpStatus.NOT_MODIFIED);
+    }
+
+    @ExceptionHandler(OptimisticLockingFailureException.class)
+    public ResponseEntity<String> handleOptimisticLockingFailureException(OptimisticLockingFailureException e) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Error-Message", "Request resource is busy: " + e.getMessage());
+        return new ResponseEntity<>("Failed to update due to concurrent modification. Please try again.", headers, HttpStatus.CONFLICT);
     }
 
 }
