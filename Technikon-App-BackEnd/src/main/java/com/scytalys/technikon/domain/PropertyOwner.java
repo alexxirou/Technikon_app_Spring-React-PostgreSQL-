f01d1b4db91e59d7b41d6881f22f6384a2e1555f
Alexandros Xirouchakis <alexandrosx@ISIH.LOCAL>
2024-05-10 22:15:14 +0300

    Updated Model Entity classes based on latest update on requirements.

---
+++ Technikon-App-BackEnd/src/main/java/com/scytalys/technikon/domain/PropertyOwner.java
@@ -0,0 +1,16 @@
+package com.scytalys.technikon.domain;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Version;
+import lombok.*;
+
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@ToString(callSuper = true)
+
+public class PropertyOwner extends User {
+
+
+
+
+}
---
+++ Technikon-App-BackEnd/src/main/java/com/scytalys/technikon/domain/PropertyRepair.java
@@ -0,0 +1,31 @@
+package com.scytalys.technikon.domain;
+
+import com.scytalys.technikon.domain.category.RepairStatus;
+import com.scytalys.technikon.domain.category.RepairType;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NonNull;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Data
+public class PropertyRepair {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private LocalDate dateOfRepair;
+    private String shortDescription;
+    private RepairType repairType;
+    private RepairStatus repairStatus;
+    private BigDecimal cost;
+    private String longDescription;
+
+    @ManyToOne
+    @NonNull
+    private Property property;
+    @ManyToOne
+    @NonNull
+    private PropertyOwner propertyOwner;
+}
