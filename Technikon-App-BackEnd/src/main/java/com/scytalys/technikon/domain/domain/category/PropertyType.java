fdd3b56907eb42c6f4dd775f0a8a8c931ecd9ae4
Alexandros Xirouchakis <alexandrosx@ISIH.LOCAL>
2024-05-10 22:09:39 +0300

    Updated Model Entity classes based on latest update on requirements.

--- Technikon-App-BackEnd/src/main/java/com/scytalys/technikon/domain/domain/category/PropertyType.java
+++
@@ -1,5 +0,0 @@
-package com.scytalys.technikon.domain.domain.category;
-
-public enum PropertyType {
-    DETACHED_HOUSE, MAISONETTE, APARTMENT_BUILDING
-}
--- Technikon-App-BackEnd/src/main/java/com/scytalys/technikon/domain/domain/category/RepairStatus.java
+++
@@ -1,5 +0,0 @@
-package com.scytalys.technikon.domain.domain.category;
-
-public enum RepairStatus {
-    SCHEDULED, IN_PROGRESS, COMPLETE, DEFAULT_PENDING
-}
