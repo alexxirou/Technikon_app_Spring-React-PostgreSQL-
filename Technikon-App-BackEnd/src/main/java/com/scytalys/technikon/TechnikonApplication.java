32a97ec4c3fd4e4b0c66379f75a419f8b0195a39
Elentina Grispou <elentina.grispou@scytalys.com>
2024-05-20 13:07:11 +0300

    Added java docs for property repair

--- Technikon-App-BackEnd/src/main/java/com/scytalys/technikon/TechnikonApplication.java
+++ Technikon-App-BackEnd/src/main/java/com/scytalys/technikon/TechnikonApplication.java
@@ -2,9 +2,10 @@ package com.scytalys.technikon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan
+@EnableCaching
 @SpringBootApplication
 public class TechnikonApplication {
     public static void main(String[] args) {
