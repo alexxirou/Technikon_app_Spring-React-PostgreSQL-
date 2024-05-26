92200c19aff4f4347f115070130d4da81258a39a
Elentina Grispou <elentina.grispou@scytalys.com>
2024-05-26 19:52:05 +0300

    Removed update from sample data

--- Technikon-App-BackEnd/src/main/java/com/scytalys/technikon/bootstrap/SampleData.java
+++ Technikon-App-BackEnd/src/main/java/com/scytalys/technikon/bootstrap/SampleData.java
@@ -134,9 +134,9 @@ public class SampleData {
             logger.info("Searched property Owner: {}", resultUser);
             List<UserSearchResponseDto> responseDto=propertyOwnerService.createSearchResponse(resultUser);
             logger.info("Created user search response: {}", responseDto);
-            UserUpdateDto newUpdate = new UserUpdateDto(null,"elsewhere","");
-            propertyOwnerService.updateUser(propertyOwner.getTin(), newUpdate);
-            logger.info("Updated user with: {}", newUpdate);
+//            UserUpdateDto newUpdate = new UserUpdateDto(null,"elsewhere","");
+//            propertyOwnerService.updateUser(propertyOwner.getTin(), newUpdate);
+//            logger.info("Updated user with: {}", newUpdate);
             resultUser =propertyOwnerService.searchUser(request );
             logger.info("Searched property Owner: {}", resultUser);
 
