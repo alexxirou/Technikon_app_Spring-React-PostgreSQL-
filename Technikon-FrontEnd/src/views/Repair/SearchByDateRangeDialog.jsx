099a71e39253f22870c16591d3bd5e026531e598
Elentina Grispou <elentina.grispou@scytalys.com>
2024-05-27 12:33:55 +0300

    Fixed text field label "Date" in searching a repair

--- Technikon-FrontEnd/src/views/Repair/SearchByDateRangeDialog.jsx
+++ Technikon-FrontEnd/src/views/Repair/SearchByDateRangeDialog.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Dialog, DialogTitle, DialogContent, DialogActions, Button, TextField } from '@mui/material';
+import { Dialog, DialogTitle, DialogContent, DialogActions, Button, TextField, Box } from '@mui/material';
 
 const SearchByDateRangeDialog = ({ open, onClose, onSearch }) => {
   const [startDate, setStartDate] = useState('');
@@ -14,22 +14,26 @@ const SearchByDateRangeDialog = ({ open, onClose, onSearch }) => {
     <Dialog open={open} onClose={onClose}>
       <DialogTitle>Search Repairs by Date Range</DialogTitle>
       <DialogContent>
-        <TextField
-          label="Start Date"
-          type="date"
-          fullWidth
-          value={startDate}
-          onChange={(e) => setStartDate(e.target.value)}
-          InputLabelProps={{ shrink: true }}
-        />
-        <TextField
-          label="End Date"
-          type="date"
-          fullWidth
-          value={endDate}
-          onChange={(e) => setEndDate(e.target.value)}
-          InputLabelProps={{ shrink: true }}
-        />
+        <Box p={2}>
+          <TextField
+            label="Start Date"
+            type="date"
+            fullWidth
+            value={startDate}
+            onChange={(e) => setStartDate(e.target.value)}
+            InputLabelProps={{ shrink: true }}
+          />
+        </Box>
+        <Box p={2}>
+          <TextField
+            label="End Date"
+            type="date"
+            fullWidth
+            value={endDate}
+            onChange={(e) => setEndDate(e.target.value)}
+            InputLabelProps={{ shrink: true }}
+          />
+        </Box>
       </DialogContent>
       <DialogActions>
         <Button onClick={onClose} color="secondary">Cancel</Button>
