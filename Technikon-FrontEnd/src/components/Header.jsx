43294cafcf9bd0d65d549d40cf4e0b5a6e87958e
Alexandros Xirouchakis <alexandrosx@ISIH.LOCAL>
2024-05-27 12:19:51 +0300

    added search user field in the header

--- Technikon-FrontEnd/src/components/Header.jsx
+++ Technikon-FrontEnd/src/components/Header.jsx
@@ -1,31 +1,76 @@
+import { useState } from 'react';
 import { Link } from "react-router-dom";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
 import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogActions from "@mui/material/DialogActions";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
 import { PATHS, ROLES } from "../lib/constants";
 import { useAuth } from '../hooks/useAuth';
 import LogoutDialog from "../components/LogoutDialog";
+import api from "../api/Api";
 
 const Header = () => {
   const { authData, logout, logoutDialogOpen, handleCloseLogoutDialog } = useAuth();
+  const [searchType, setSearchType] = useState('tin');
+  const [searchValue, setSearchValue] = useState('');
+  const [searchResult, setSearchResult] = useState([]);
+  const [searchDialogOpen, setSearchDialogOpen] = useState(false);
+
+  const handleSearch = async () => {
+    const searchObject = {
+      tin: null,
+      username: null,
+      email: null,
+    };
+
+    searchObject[searchType] = searchValue.trim() === '' ? null : searchValue;
+
+    try {
+      const response = await api.get('/api/propertyOwners/', { params: searchObject });
+      if (response.status===200){
+      const data = response.data;
+      setSearchResult(data);
+      setSearchDialogOpen(true);
+    } else {
+      const errorMessage = response.data.message || 'Login failed: Invalid response';
+      throw new Error(errorMessage);
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    if (error.response) {
+      const errorMessage = error.response.data || 'Login failed: Invalid response';
+      setError(errorMessage);
+    } else if (error.request) {
+      setError('No response received from the server');
+    } else {
+      setError('An unexpected error occurred');
+    }
+  }
+  };
+
+  const handleCloseSearchDialog = () => {
+    setSearchDialogOpen(false);
+  };
+
+  const handleGoToOwnerDetails = (id) => {
+    setSearchDialogOpen(false); 
+    window.location.href = PATHS.OWNER(id);
+  };
 
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="open navigation menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography
             variant="h6"
             component={Link}
@@ -38,47 +83,43 @@ const Header = () => {
           >
             Home
           </Typography>
-          <Button variant="contained" color="primary" onClick={handleSearch}>
-            Search
-          </Button>
-          <TextField
-            label="Search"
-            variant="outlined"
-            size="small"
-            value={searchValue}
-            onChange={(e) => setSearchValue(e.target.value)}
-            InputProps={{ sx: { backgroundColor: 'white' } }}
-
-            sx={{ mr: 2, ml:2 }}
-          />
-
-
-
-
-<Select
-  value={searchType}
-  onChange={(e) => setSearchType(e.target.value)}
-  size="small"
-
-  sx={{
-    mr: 2,
-  }}
->
-  <MenuItem value="tin">TIN</MenuItem>
-  <MenuItem value="username">Username</MenuItem>
-  <MenuItem value="email">Email</MenuItem>
-</Select>
-
-
-
-
+          {authData && (
+            <>
+              <Select
+                value={searchType}
+                onChange={(e) => setSearchType(e.target.value)}
+                size="small"
+                sx={{ mr: 2 }}
+              >
+                <MenuItem value="tin">TIN</MenuItem>
+                <MenuItem value="username">Username</MenuItem>
+                <MenuItem value="email">Email</MenuItem>
+              </Select>
+              <TextField
+                label="Search"
+                variant="outlined"
+                size="small"
+                value={searchValue}
+                onChange={(e) => setSearchValue(e.target.value)}
+                InputProps={{
+                  sx: {
+                    backgroundColor: 'white',
+                  },
+                }}
+                sx={{ mr: 2 }}
+              />
+              <Button variant="contained" color="primary" onClick={handleSearch}>
+                Search
+              </Button>
+            </>
+          )}
+          {authData && authData.authorities.includes(ROLES.ADMIN) && (
+            <Button component={Link} to={PATHS.ADMIN} color="inherit">
+              Admin
+            </Button>
+          )}
           {authData ? (
             <>
-              {authData.authorities.includes(ROLES.ADMIN) && (
-                <Button component={Link} to={PATHS.ADMIN} color="inherit">
-                  Admin
-                </Button>
-              )}
               <Button component={Link} to={PATHS.OWNER(authData.userId)} color="inherit">
                 Owner
               </Button>
@@ -119,15 +160,17 @@ const Header = () => {
                   <Typography><strong>Email:</strong> {result.email}</Typography>
                 </CardContent>
               </Card>
-              <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: '1rem' }}>
-                <Button
-                  onClick={() => handleGoToOwnerDetails(result.id)}
-                  color="primary"
-                  variant="contained"
-                >
-                  Go to Owner Details
-                </Button>
-              </Box>
+              {authData && authData.authorities.includes(ROLES.ADMIN) && (
+                <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: '1rem' }}>
+                  <Button
+                    onClick={() => handleGoToOwnerDetails(result.id)}
+                    color="primary"
+                    variant="contained"
+                  >
+                    Go to Owner Details
+                  </Button>
+                </Box>
+              )}
             </Box>
           ))}
         </DialogContent>
@@ -137,9 +180,6 @@ const Header = () => {
           </Button>
         </DialogActions>
       </Dialog>
-
-
-
     </Box>
   );
 };
